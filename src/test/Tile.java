1dc60a5824c7437d172e12013956a672614649e6
evya61992 <evya61992@gmail.com>
2024-03-25 18:18:04 +0200

    tile started

--- src/test/Tile.java
+++ src/test/Tile.java
@@ -1,8 +1,35 @@
 package test;
 
+import java.util.Objects;
+
 public class Tile {
+    // Attributes
+    public final char letter;
+    public final int score;
+    //CTORS
+    public Tile(char l, int s){
+        this.letter=l;
+        this.score=s;
+    }
+	//Methods
+    public char getLetter() {
+        return letter;
+    }
 
+    public int getScore() {
+        return score;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return letter == tile.letter && score == tile.score;
+    }
 
-	
+    @Override
+    public int hashCode() {
+        return Objects.hash(letter, score);
+    }
 }
