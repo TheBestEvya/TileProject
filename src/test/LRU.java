f17486a8dcd8b3c749d7b85c85cb9d404b2c848d
evya61992 <evya61992@gmail.com>
2024-05-16 17:21:59 +0300

    LRU and LFU are done!

--- src/test/LRU.java
+++ src/test/LRU.java
@@ -1,6 +1,24 @@
 package test;
 
 
-public class LRU {
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 
+public class LRU implements CacheReplacementPolicy {
+
+    private LinkedHashSet<String> words = new LinkedHashSet<>();
+    @Override
+    public void add(String word) {
+    words.remove(word);
+    words.add(word);
+    }
+
+    @Override
+    public String remove() {
+        Iterator<String> itr = words.iterator();
+        String temp = itr.next();
+        words.remove(itr.next());
+
+        return temp;
+    }
 }
