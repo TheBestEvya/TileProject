8119e53a159ada66ff514c0ceca6a021ec57153a
evya61992 <evya61992@gmail.com>
2024-05-16 18:51:20 +0300

    started server need to complete runServer

--- src/test/MyServer.java
+++ src/test/MyServer.java
@@ -1,13 +1,46 @@
 package test;
 
 
+import com.sun.security.ntlm.Server;
+
+import java.net.ServerSocket;
+
 public class MyServer {
 
     private int port;
-    private ClientHandler cl;
+    private ClientHandler ch;
+    private boolean stop;
+
 
     public MyServer(int p, ClientHandler c){
+    this.port = p;
+    this.ch = c;
+    this.stop=false;
+    }
+
+    public void start(){
+//    new Thread(()->runServer()).start();
+    runServer();
+    }
+
+	private void runServer() throws Exception{
+        ServerSocket server = new ServerSocket(this.port);
+        server.setSoTimeout(1000);
+        while(!stop){
+            try{
+
+
+
+            }catch (Exception e){
+
+            }
+
+
+
+        }
 
     }
-	
+    public void stop(){
+        this.stop=true;
+    }
 }
